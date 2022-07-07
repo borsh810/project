8dcf605f422709a3ae43c8ba5949c817d17945f5
Bohdan <example@gmail.com>
2022-07-07 15:26:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', '18');
+// console.log((answer + 5));
+
+// вся информация которую мы будем получать от пользователя будет строкой
+
+// const answers = [];
+
+// answers[0] = prompt('kak vashe imia?', '');
+// answers[1] = prompt('kak vashe fam?', '');
+// answers[2] = prompt('kak vashe let?', '');
+
+// console.log(typeof(answers));
+
+// массив это не отдельный тип данных
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Privet, ${user}`);
+
+// console.log('arr' + ' -ob');
+
+// let incr = 10,
+// 	decr = 10;
+
+// 	console.log(incr++);
+// 	console.log(decr--);
+
+// 	const isYes = true,
+// 			isHah = false;
+
+// 			console.log(isYes && isHah);
+
