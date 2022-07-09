b9f53587abc1f85746f4f91161c14530dd363ece
Bohdan <example@gmail.com>
2022-07-09 13:23:22 +0300

    018 script

--- js/script.js
+++ js/script.js
@@ -90,12 +90,79 @@
 // 	num++;
 // }
 // while(num < 55);
-let num = 50;
-for (let i = 1; i < 10; i++) {
-	if(i === 6) {
-		// break;
-		continue;
-	}
-	console.log(i);
+// let num = 50;
+// for (let i = 1; i < 10; i++) {
+// 	if(i === 6) {
+// 		// break;
+// 		continue;
+// 	}
+// 	console.log(i);
 	
-}
+// }
+
+// Функции
+// let num = 20;
+
+// function showFirstMessage(text) {
+// 	console.log(text);
+// 	console.log(num);
+// }
+
+// showFirstMessage('Hello World');
+// console.log(num);
+
+// console.log(calc(4,3));
+// console.log(calc(5,6));
+// console.log(calc(10,6));
+
+// function calc(a,b) {
+// 	return (a + b);
+// }
+
+// function ret() {
+// 	let num = 50;
+
+
+
+// 	return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+// 	console.log("Hello");
+// };
+
+// logger();
+
+// const calc = (a,b) => {
+// 	console.log('1');
+// 	return a + b;
+// };
+
+// Методы и свойства
+
+const str = 'teSt';
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6,11));
+
+console.log(logg.substring(6,11));
+
+console.log(logg.substr(6,5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2';
+console.log(parseInt(test));
+console.log(parseFloat(test));
